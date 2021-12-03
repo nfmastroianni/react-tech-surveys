5a21c93a95d3d526581ccec5b23c38674228942a
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-12-03 09:26:05 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import cookies from "js-cookie";
+import ReactCountryFlag from "react-country-flag";
+import "./App.css";
 
 function App() {
+  const [language, setLanguage] = useState("en");
+  useEffect(() => {
+    setLanguage(cookies.get("i18next"));
+    console.log(language);
+  }, [language]);
+  const { t } = useTranslation();
+  const languages = [
+    {
+      code: "en",
+      name: "English",
+      country_code: "US",
+    },
+    {
+      code: "es",
+      name: "Español",
+      country_code: "ES",
+    },
+    {
+      code: "pr",
+      name: "Portuguese",
+      country_code: "BR",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "relative" }}>
+      <div className="background--custom uk-flex uk-flex-center uk-flex-middle">
+        <header>
+          <div className="uk-inline">
+            <button className="uk-button uk-button-danger" type="button">
+              {language === "en" && <ReactCountryFlag countryCode="US" />}
+              {language}
+            </button>
+            <div data-uk-dropdown="mode: click">
+              <ul>
+                {languages.map((lang) => {
+                  return (
+                    <li
+                      key={lang.name}
+                      style={{ listStyle: "none" }}
+                      className=""
+                    >
+                      <ReactCountryFlag countryCode={lang.country_code} />
+                      {lang.name}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </header>
+        <div className="uk-container uk-container-medium uk-text-center">
+          <h1 className="welcome uk-heading-medium" style={{ color: "#fff" }}>
+            {t("welcome")}
+            <br />
+            {t("organization")}
+            <br />
+            {t("purpose")}
+          </h1>
+        </div>
+        <footer className="">
+          <img
+            src="https://www.longbranch.k12.nj.us/cms/lib/NJ01001766/Centricity/Template/GlobalAssets/images///logos/LBPS%20Seal%20-%20Together%20We%20Can.png"
+            alt=""
+            style={{ width: "65px" }}
+          />{" "}
+          <p>
+            {t("organization")}
+            <br />"{t("slogan")}"
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import "uikit/dist/css/uikit.min.css";
+import "uikit/dist/js/uikit.min.js";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    supportedLngs: ["en", "es", "pr"],
+    fallbackLng: "en",
+    detection: {
+      order: [
+        "path",
+        "querystring",
+        "cookie",
+        "localStorage",
+        "sessionStorage",
+        "navigator",
+        "htmlTag",
+      ],
+      caches: ["cookie", "localStorage"],
+    },
+    backend: {
+      loadPath: "/assets/locales/{{lng}}/translation.json",
+    },
+  });
+
+const loadingMarkup = (
+  <div
+    className="background--custom"
+    style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#064e3b",
+    }}
+  >
+    <div style={{ maxWidth: "600px" }}>
+      <span uk-spinner="ratio: 4.5"></span>
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+  document.getElementById("root")
+);
